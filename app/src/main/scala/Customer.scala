692c7a2eb4c78cf7863ccff6d5ba729bfddd209e
Graham Berks <graham@fatlazycat.com>
2021-05-05 16:28:33 +0100

    More fixes

--- app/src/main/scala/Customer.scala
+++ app/src/main/scala/Customer.scala
@@ -16,18 +16,18 @@ class Customer(val name: String) {
     rentals.forEach { rental =>
       var thisAmount: Double = 0
       // determines the amount for each line
-      rental.getMovie.getPriceCode match {
+      rental.movie.getPriceCode match {
         case Movie.REGULAR =>
           thisAmount += 2
-          if (rental.getDaysRented > 2) thisAmount += (rental.getDaysRented - 2) * 1.5
+          if (rental.daysRented > 2) thisAmount += (rental.daysRented - 2) * 1.5
         case Movie.NEW_RELEASE =>
-          thisAmount += rental.getDaysRented * 3
+          thisAmount += rental.daysRented * 3
         case Movie.CHILDRENS =>
           thisAmount += 1.5
-          if (rental.getDaysRented > 3) thisAmount += (rental.getDaysRented - 3) * 1.5
+          if (rental.daysRented > 3) thisAmount += (rental.daysRented - 3) * 1.5
       }
       frequentRenterPoints += 1
-      if ((rental.getMovie.getPriceCode eq Movie.NEW_RELEASE) && rental.getDaysRented > 1) frequentRenterPoints += 1
+      if ((rental.movie.getPriceCode eq Movie.NEW_RELEASE) && rental.daysRented > 1) frequentRenterPoints += 1
       result += "\t" + rental.getMovie.getTitle + "\t" + String.valueOf(thisAmount) + "\n"
       totalAmount += thisAmount
     }
